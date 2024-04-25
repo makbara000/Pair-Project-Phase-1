75437e94479066c3b374537016f7dc60f100f54b
Hasbi Firasyan <firasyanhasbi@gmail.com>
2024-04-25 15:03:18 +0700

    added new ejs view home and register

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,23 @@ router.get('/', (req, res) => {
     res.render('Login')
   });
 
+router.get('/register', (req, res) => {
+    res.render('register')
+  });
+router.post('/register', (req, res) => {
+    res.render('Login')
+  });
+
+router.get('/login', (req, res) => {
+    res.render('Login')
+  });
+router.post('/login', (req, res) => {
+    res.render('Login')
+  });
+
+router.get('/users', (req, res) => {
+    res.render('users')
+  });
+
 
 module.exports=router;
