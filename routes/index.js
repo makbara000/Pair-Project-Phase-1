bd132b7fe89abc80737e8e178725ab3b1c575668
Hasbi Firasyan <firasyanhasbi@gmail.com>
2024-04-25 16:51:20 +0700

    routing done

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,8 @@ const logMiddleware = require('../middlewares/log.middleware');
 
 const router = require('express').Router();
 
-
-router.get('/', (req, res) => {
-  res.render('Home')
-});
-
+//|-----AUTHENTICATION------|
+//register
 router.get('/register', (req, res) => {
   res.render('Register')
 });
@@ -14,17 +11,51 @@ router.post('/register', (req, res) => {
   res.render('Register')
 });
 
+//login
 router.get('/login', (req, res) => {
   res.render('Login')
 });
 router.post('/login', (req, res) => {
   res.render('Login')
 });
+//logout
+router.get("/logout");
+//-----------
+
+
+
+
+
+//|-----MIDDLEWARE------|
 router.use(logMiddleware)
+//-----------
 
-router.get('/users', (req, res) => {
-    res.render('users')
-  });
 
+
+
+//|--user:-investor------|
+router.get('/'); //user home, showStocklist
+router.get('/cart');
+
+router.get('/cart/increase/:id');
+router.get('/cart/decrease/:id');
+router.post('/cart/buy/:id');
+
+router.get('/user/edit/profile/:id');
+router.post('/user/edit/profile/:id');
+router.get('/addToCart/:id');
+//-----------
+
+
+
+
+//|--user:-admin--------|
+router.get('/admin');//adminPage
+router.get('/admin/addStock');//getFormAddStock
+router.post('/admin/addStock');//postFormAddStock
+router.get("/admin/editStock/:id");//getEdit;
+router.post("/admin/editStock/:id");//postEdit
+router.get("/admin/deleteStock/:id");//destroyProduct;
+//-----------
 
 module.exports=router;
