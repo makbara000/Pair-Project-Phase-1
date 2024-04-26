902ff27c9c2a432abd4b5aea2f6617c8552d558c
makbara000 <makbara000@gmail.com>
2024-04-26 08:30:31 +0700

    Pair Project kelar

--- helper/emailer.js
+++ helper/emailer.js
@@ -1,1 +1,21 @@
-const nodemailer = require('nodemailr')
+const nodemailer = require('nodemailer');
+function 
+const transporter = nodemailer.createTransport({
+    service:'gamil',
+    auth: {
+        user: 'youremail@gmail.com',
+        pass: 'your password'
+    }
+});
+
+let mailOptions = {
+    from: 'youremail@gmail.com',
+    to: 'youremail@gmail.com',
+    subject: 'Sending email through SINTETIK',
+    text: 'That was Easy'
+};
+
+transporter.sendMail(mailOptions, (err,info)=>{
+    if(err) throw err;
+    console.log('Email sent: ' + info.response)
+})
